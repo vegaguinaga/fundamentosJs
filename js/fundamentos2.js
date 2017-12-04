250ecd1612d4ec59802de7d8a4860fb83c0c9c6c
Jose Vega <vegaguinaga@gmail.com>
2017-12-03 21:45:49 -0600

    Mas JavaScript

--- js/fundamentos2.js
+++ js/fundamentos2.js
@@ -2,6 +2,8 @@
  var boton = document.getElementById("boton");
  var boton2 = document.getElementById("boton2");
 
+ var numero = document.getElementById("numero");
+
  //DECLARACION DE FUNCIONES
  function eventoClick(evento)
  {
@@ -11,11 +13,43 @@
 
  	evento.target.style.borderRadius = '1em';
  	evento.target.style.fontSize = "2em";
+
+ 	boton2.removeEventListener("click", eventoClick);
+ 	boton2.addEventListener("dblclick", otroEventoClick);
  }
 
+function otroEventoClick(evento)
+{
+	alert("Has presionado el botón en el evento '" + evento.type + "' con el objeto de nombre id '" + evento.target.id +"'");
+ 	console.log(evento);
+
+ 	evento.target.style.background = "black";
+ 	evento.target.style.color = "white";
+}
+
+function parImpar()
+{
+	var numero = prompt("Ingresa un número");
+
+	//isNaN- is Not a Number true si el valor es  alfanum , false si es númerico
+
+	if (isNaN(numero)){
+		alert("No me engañes, eso no es un número");
+	} else {
+		var modulo = numero % 2;
+		var tipo =(modulo == 0) ? "Par" : "Impar";
+		alert("El número "+numero+" es "+tipo);
+		
+	}
+}
  // ASIGNACION DE EVENTOS
  //Los manejadores de eventos semanticos se ejecutan a la carga del documento
 
  window.onload = function(){
  	boton.onclick = eventoClick;
+ 	boton.onclick = otroEventoClick;
+
+ 	boton2.addEventListener("click", eventoClick);
+
+ 	numero.addEventListener("click", parImpar);
  }
